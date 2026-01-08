3805e808536988eff7e93b697caa8f2b9549ca6a
wyh <dzzwmqj@qq.com>
2026-01-08 17:13:42 +0800

    feat: 完成p5垂直排列扫描

--- Drivers/BSP/Display/scan.c
+++ Drivers/BSP/Display/scan.c
@@ -105,6 +105,61 @@ void convert_pixelmap_p10(void)
     }
 }
 
+#define CHANNEL_NUM       (MODULE_PER_ROW * MOUDLE_CHANNEL_NUM)                                       // 总通道数
+#define CHANNEL_PIXEL_NUM (MODULE_PIXEL_ROW * MODULE_PIXEL_COL * MODULE_PER_COL / MOUDLE_CHANNEL_NUM) // 单个通道的像素个数
+const ChannelStruct_TypeDef channel_red[10] = {
+    {R2_GPIO_Port, R2_Pin},
+    {R1_GPIO_Port, R1_Pin},
+    {R4_GPIO_Port, R4_Pin},
+    {R3_GPIO_Port, R3_Pin},
+    {R6_GPIO_Port, R6_Pin},
+    {R5_GPIO_Port, R5_Pin},
+    {R8_GPIO_Port, R8_Pin},
+    {R7_GPIO_Port, R7_Pin},
+    {R10_GPIO_Port, R10_Pin},
+    {R9_GPIO_Port, R9_Pin},
+};
+const ChannelStruct_TypeDef channel_green[10] = {
+    {G2_GPIO_Port, G2_Pin},
+    {G1_GPIO_Port, G1_Pin},
+    {G4_GPIO_Port, G4_Pin},
+    {G3_GPIO_Port, G3_Pin},
+    {G6_GPIO_Port, G6_Pin},
+    {G5_GPIO_Port, G5_Pin},
+    {G8_GPIO_Port, G8_Pin},
+    {G7_GPIO_Port, G7_Pin},
+    {G10_GPIO_Port, G10_Pin},
+    {G9_GPIO_Port, G9_Pin},
+};
+const ChannelStruct_TypeDef channel_blue[10] = {
+    {B2_GPIO_Port, B2_Pin},
+    {B1_GPIO_Port, B1_Pin},
+    {B4_GPIO_Port, B4_Pin},
+    {B3_GPIO_Port, B3_Pin},
+    {B6_GPIO_Port, B6_Pin},
+    {B5_GPIO_Port, B5_Pin},
+    {B8_GPIO_Port, B8_Pin},
+    {B7_GPIO_Port, B7_Pin},
+    {B10_GPIO_Port, B10_Pin},
+    {B9_GPIO_Port, B9_Pin},
+};
+/**
+ * @brief p5垂直排列扫描
+ *
+ */
+void convert_pixelmap_p5v(void)
+{
+    uint16_t row_cnt = 0, col_cnt = 0, buff_cnt = 0;
+
+    for (uint16_t src_cnt = 0; src_cnt < DISRAM_SIZE; src_cnt++) {
+        row_cnt = src_cnt / SCREEN_PIXEL_ROW; // 屏幕的行标
+        col_cnt = src_cnt % SCREEN_PIXEL_ROW;
+
+        buff_cnt             = ((col_cnt / 16 + 1) * 8 - 1 - col_cnt % 8) * SCAN_LINE_PIXEL_NUM + row_cnt * 2 + col_cnt / 8 % 2;
+        hub75_buff[buff_cnt] = pixel_map[src_cnt];
+    }
+}
+
 /**
  * @brief 更新点阵缓冲区到显存
  *
